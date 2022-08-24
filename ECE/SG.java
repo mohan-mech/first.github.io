9e113da4c5f5ed95bdaed01e28a1121ed4ef8b2e
mohan-mech <111970317+mohan-mech@users.noreply.github.com>
2022-08-24 15:22:54 +0530

    Add files via upload

---
+++ ECE/SG.java
@@ -0,0 +1,16 @@
+
+import java.applet.*;
+import java.awt.*;
+/*<applet code=Appletex width=400 height=500><1 applet>*/
+public class  Appletex extands Applet 
+{
+public void init()
+{
+setBackground (Color.black);
+setForeground (Color.yellow);
+}
+public void paint(Grahics g)
+{
+g.drawSrting("welcome",40,40);
+}
+}
